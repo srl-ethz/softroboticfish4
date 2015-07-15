17f815693e52a0d9df3a288124f5efe6532e5168
Ankur Mehta <mehtank@csail.mit.edu>
2015-07-15 14:03:19 -0400

    made OOK tx a usable class

--- ooktx.py
+++ ooktx.py
@@ -14,9 +14,9 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from optparse import OptionParser
 
-class top_block(gr.top_block):
+class OOKTX(gr.top_block):
 
-    def __init__(self, txstr, carrier=10000, samp_rate = 80000, bw=4000, amp=1):
+    def __init__(self, txstr="Hello world", carrier=32000, samp_rate = 80000, bw=1000, amp=1):
         gr.top_block.__init__(self, "Top Block")
 
         ##################################################
@@ -24,37 +24,47 @@ class top_block(gr.top_block):
         ##################################################
         self.samp_rate = samp_rate 
         self.carrier = carrier 
+        self.bw = bw
 
         ##################################################
         # Blocks
         ##################################################
-        self.blocks_vector_source_x_1 = blocks.vector_source_b(tuple(bytearray(txstr)), False, 1, [])
-        self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(1, gr.GR_LSB_FIRST)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc(([0,1,1,0]), 2)
+        self.source = blocks.vector_source_b(tuple(bytearray(txstr)), False, 1, [])
+        blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(1, gr.GR_LSB_FIRST)
+        digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc(([0,1,1,0]), 2)
 
-        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, samp_rate/bw)
+        blocks_repeat_0 = blocks.repeat(gr.sizeof_gr_complex*1, samp_rate/bw)
         #XXX Hack: 0.07 should actually be parameter amp, but RPI crashes
-        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, carrier, 0.07, 0)
-        self.blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
-        self.blocks_complex_to_real_0 = blocks.complex_to_real(1)
-        self.audio_sink_0 = audio.sink(samp_rate, "")
+        analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, carrier, 0.07, 0)
+        blocks_multiply_xx_0_0 = blocks.multiply_vcc(1)
+        blocks_complex_to_real_0 = blocks.complex_to_real(1)
+        audio_sink_0 = audio.sink(samp_rate, "")
+        
+        self.samp_blocks = [self.source, 
+                            blocks_repeat_0,
+                            analog_sig_source_x_0,
+                            audio_sink_0]
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_vector_source_x_1, 0), (self.blocks_packed_to_unpacked_xx_0, 0))
-        self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.source, 0), (blocks_packed_to_unpacked_xx_0, 0))
+        self.connect((blocks_packed_to_unpacked_xx_0, 0), (digital_chunks_to_symbols_xx_0, 0))
+        self.connect((digital_chunks_to_symbols_xx_0, 0), (blocks_repeat_0, 0))
 
-        self.connect((self.blocks_repeat_0, 0), (self.blocks_multiply_xx_0_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0_0, 1))
-        self.connect((self.blocks_multiply_xx_0_0, 0), (self.blocks_complex_to_real_0, 0))
-        self.connect((self.blocks_complex_to_real_0, 0), (self.audio_sink_0, 0))
+        self.connect((blocks_repeat_0, 0), (blocks_multiply_xx_0_0, 0))
+        self.connect((analog_sig_source_x_0, 0), (blocks_multiply_xx_0_0, 1))
+        self.connect((blocks_multiply_xx_0_0, 0), (blocks_complex_to_real_0, 0))
+        self.connect((blocks_complex_to_real_0, 0), (audio_sink_0, 0))
+
+    def send(self, txstr):
+        self.source.set_data(tuple(bytearray(txstr)), [])
+        self.start()
+        self.wait()
 
 def send(txstr, carrier, samp_rate, bw, amp):
-    tb = top_block(txstr, carrier, samp_rate, bw, amp)
-    tb.start()
-    tb.wait()
+    tb = OOKTX(txstr, carrier, samp_rate, bw, amp)
+    tb.send(txstr)
 
 if __name__ == '__main__':
     parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
