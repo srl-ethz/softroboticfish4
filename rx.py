37b63071658df25992597a6b7980902e92be634c
Ankur Mehta <mehtank@csail.mit.edu>
2015-07-16 16:57:03 -0400

    killable rx, decoding of chip sequences

--- rx.py
+++ rx.py
@@ -4,6 +4,9 @@ import numpy as np
 from scipy.signal import decimate
 from rtlsdr import RtlSdr, limit_calls
 from codes import manchester, codes154, codes2corr
+import matplotlib.pyplot as plt
+
+import sys, select
 
 class Demod:
   SAMP_RATE = 1024e3
@@ -23,7 +26,7 @@ class Demod:
     self.bw = bw
     self.sps = sps
 
-    ts = np.arange(Demod.SAMP_WINDOW)/Demod.SAMP_RATE
+    ts = np.arange(Demod.SAMP_WINDOW*2)/Demod.SAMP_RATE
     self.mixer = np.exp(-2*np.pi*carrier*ts*1j)
 
     decim = Demod.SAMP_RATE/self.bw/self.sps
@@ -31,11 +34,11 @@ class Demod:
     self.decim = int(decim)
     assert Demod.SAMP_WINDOW % self.decim == 0
 
+    self.sampchips = Demod.SAMP_WINDOW / self.decim 
     self.corr = codes2corr(codes, sps)
     self.codelen = len(self.corr[0])
 
     self.last = np.zeros(Demod.SAMP_WINDOW)
-    self.index = 0
 
   def bb2c(self, baseband):
     mag = np.abs(baseband)
@@ -50,41 +53,81 @@ class Demod:
     return corrs
 
   def ddc(self, samp, sdr):
-    assert len(samp) == len(self.mixer)
-    overlap = self.decim * self.codelen
-    print self.codelen
-    print overlap
-    print len(samp)
-    print 1/0
-    extsamp = np.concatenate((self.last[-2*overlap:], samp))
+    extsamp = np.concatenate((self.last, samp))
     self.last = samp
     baseband = decimate(extsamp * self.mixer, self.decim, ftype='fir')
-    st = int(self.codelen/2)
-    end = st+len(samp)+self.codelen
-    mag, phase, dp  = self.bb2c(baseband[st:end])
+    mag, phase, dp  = self.bb2c(baseband)
     corrs = self.decode(mag)
 
-    self.chips[self.index*len(chips):(self.index+1)*len(chips)] = chips
-    self.chips[self.index*len(chips):(self.index+1)*len(chips)] = corrs[0]
+    self.chips[self.index*self.sampchips:(self.index+1)*self.sampchips] = mag[self.codelen:self.codelen+self.sampchips]
+    self.demod[self.index*self.sampchips:(self.index+1)*self.sampchips] = corrs[0][self.codelen:self.codelen+self.sampchips]
     self.index += 1
+
+  def end(self):
+    self.sdr.close()
 
   def run(self, limit):
     self.chips = np.zeros(limit * Demod.SAMP_WINDOW / self.decim)
     self.demod = np.zeros(limit * Demod.SAMP_WINDOW / self.decim)
+    self.index = 0
 
     @limit_calls(limit)
     def callback(samp, sdr):
+      if select.select([sys.stdin], [], [], .000001)[0]:
+        sdr.cancel_read_async()
       self.ddc(samp, sdr)
 
     self.sdr.read_samples_async(callback, Demod.SAMP_WINDOW)
+    print self.index, "samples read"
+
+  def plot(self):
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.plot(self.chips)
+    ax2 = fig.add_subplot(212, sharex=ax1)
+    ax2.plot(self.demod)
+    plt.show()
+
+  def checkdemod(self, index, packetlen=5):
+    l = packetlen*8+6+7
+    b = self.demod[index:index+l*self.codelen:self.codelen]
+    print chipsToString(np.concatenate(([-1,1], b, [1])))
+
+def chipsToString(bits):
+  char = 0
+  rxstr = ""
+  for i, b in enumerate(bits):
+    char += 0 if b > 0 else (1 << (i%8))
+    if i % 8 == 7:
+      rxstr += chr(char)
+      char = 0
+  return rxstr, char
+  
+def decode(d):
+  header = [1, 1, 1, -1, -1, 1]
+  for j in range(8):
+    b = np.sign(d[j::8])
+    hind = 0
+    hloc = 0
+
+    for i, bit in enumerate(b):
+      if hind < len(header):
+        if bit == header[hind]:
+          hind += 1
+          if hind == len(header):
+            hloc = (8*i+j-6)
+            rxstr, tail = chipsToString(b[i+1:i+48])
+            if tail == 120:
+              print "header found at %d in bin %d: " % (hloc, j), rxstr
+            hind = 0
+        else:
+          hind = 0
 
 if __name__ == "__main__":
-  d = Demod(carrier=10000, bw=100, sps=4, codes = manchester)
-  d.run(1)
+  d = Demod(carrier=32000, bw=1000, sps=4, codes = manchester)
+  d.run(50)
 
-  import matplotlib.pyplot as plt
-  p = plt.plot(d.chips)
-  plt.show()
+  decode(d.demod)
 
   '''
   dx = len(d.corr[0]*8)
