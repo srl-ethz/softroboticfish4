3d2fdf667c21130cf3738d04df691ba6116de968
Ankur Mehta <mehtank@csail.mit.edu>
2015-07-22 15:37:17 -0400

    Storing javascript state and generating uncoded bit string

--- python/evjs.py
+++ python/evjs.py
@@ -1,7 +1,4 @@
-import time, pprint
 from evdev import InputDevice, categorize, ecodes, list_devices
-
-dev = InputDevice(list_devices()[0])
 
 class KeyEvent:
   def __init__(self):
@@ -11,6 +8,7 @@ class KeyEvent:
     self.numpush = 0
 
   def update(self, event):
+    retval = False
     value = event.value
     if event.type == ecodes.EV_ABS:
       value = 0 if abs(event.value - 127)<10 else 1
@@ -18,36 +16,91 @@ class KeyEvent:
     if self.lastts and self.lastval == 1 and value == 0:
       self.numpush += 1
       self.totalts += event.timestamp() - self.lastts
+      retval = True
+
     self.lastval = value
     self.lastts = event.timestamp()
+    return retval
 
   def __repr__(self):
     return "%d : %f" % (self.numpush, self.totalts)
 
-keys = {}
+class Channel:
+  def __init__(self, minval, maxval, bits, upkey, dnkey):
+    self.minval = minval
+    self.maxval = maxval
+    self.bits = bits
+    self.upkey = upkey
+    self.dnkey = dnkey
 
-while True:
-  try:
-    for event in dev.read():
-      key = event.code
-      if event.type == ecodes.EV_ABS:
-        if abs(event.value - 127) < 10:
-          try:
-            keys[key + 0.1].update(event)
-          except KeyError:
-            keys[key + 0.1] = KeyEvent()
-          try:
-            keys[key + 0.2].update(event)
-          except KeyError:
-            keys[key + 0.2] = KeyEvent()
-        key += 0.1 if event.value < 128 else 0.2
-      if event.type in [ecodes.EV_ABS, ecodes.EV_KEY]:
-        try:
-          keys[key].update(event)
-        except KeyError:
-          keys[key] = KeyEvent()
-      pprint.pprint(keys)
-  except IOError:
-    pass
+    self.value = 0
 
-  time.sleep(0.01)
+  def press(self, key):
+    if key == self.upkey:
+      self.value = min(self.maxval, self.value + 1)
+    elif key == self.dnkey:
+      self.value = max(self.minval, self.value - 1)
+
+  def tobits(self):
+    s = "{:0%db}" % self.bits
+    b = s.format(self.value & ((1 << self.bits) - 1))
+    return map(int, b)
+
+class Joystick:
+  def __init__(self, device=0):
+    self.keys = {}
+    self.dev = InputDevice(list_devices()[device])
+
+    self.S = Channel( 0, 1, 1, 295, 294) # select, start
+    self.P = Channel(-3, 3, 3, 1.1, 1.2) # up, down
+    self.Y = Channel(-3, 3, 3, 0.2, 0.1) # left, right
+    self.T = Channel( 0, 3, 2, 290, 289) # X, B
+    self.F = Channel( 0, 3, 2, 288, 291) # A, Y
+    self.spytf = (self.S, self.P, self.Y, self.T, self.F)
+
+  def tobits(self):
+    bits = []
+    for k in self.spytf:
+      bits += k.tobits()
+    return bits
+
+  def pressed(self, key):
+    print "pressed ", key
+    for k in self.spytf:
+      k.press(key)
+
+  def scan(self):
+    try:
+      for event in self.dev.read():
+        key = event.code
+        if event.type == ecodes.EV_ABS:
+          if abs(event.value - 127) < 10:
+            try:
+              if self.keys[key + 0.1].update(event):
+                self.pressed(key + 0.1)
+            except KeyError:
+              self.keys[key + 0.1] = KeyEvent()
+            try:
+              if self.keys[key + 0.2].update(event):
+                self.pressed(key + 0.2)
+            except KeyError:
+              self.keys[key + 0.2] = KeyEvent()
+          key += 0.1 if event.value < 128 else 0.2
+        if event.type in [ecodes.EV_ABS, ecodes.EV_KEY]:
+          try:
+            if self.keys[key].update(event):
+              self.pressed(key)
+          except KeyError:
+            self.keys[key] = KeyEvent()
+    except IOError:
+      pass
+
+if __name__ == "__main__":
+  import time, pprint
+
+  j = Joystick()
+  while True:
+    j.scan()
+    print j.tobits()
+    time.sleep(0.5)
+
